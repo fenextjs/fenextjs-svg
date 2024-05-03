90eea5dd9dccac3fc7d82b86e03bb95565f64585
Francisco Blanco <blancofrancisco34@gmail.com>
2024-05-03 11:35:48 -0500

    flb

--- cjs/View/TableBox/_.stories.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("@storybook/types").Args>;
-export default _default;
-export declare const Index: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, {
-    className?: string | undefined;
-}>;
